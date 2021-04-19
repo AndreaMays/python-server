fb73de14f592a0d9a198dda6257a0e231d70c9bc
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-04-19 10:39:49 -0500

    Finished chapter 14 animals. Still need to do embed customer and embed employee locations

--- animals/request.py
+++ animals/request.py
@@ -1,6 +1,7 @@
 import sqlite3
 import json
 from models import Animal
+from models import Location
 
 ANIMALS = [
     {
@@ -100,14 +101,18 @@ def get_all_animals():
 
         # Write the SQL query to get the information you want
         db_cursor.execute("""
-        SELECT
-            a.id,
-            a.name,
-            a.breed,
-            a.status,
-            a.location_id,
-            a.customer_id
-        FROM animal a
+            SELECT
+                a.id,
+                a.name,
+                a.breed,
+                a.status,
+                a.location_id,
+                a.customer_id,
+                l.name location_name,
+                l.address location_address
+            FROM Animal a
+            JOIN Location l
+            ON l.id = a.location_id
         """)
 
         # Initialize an empty list to hold all animal representations
@@ -116,18 +121,31 @@ def get_all_animals():
         # Convert rows of data into a Python list
         dataset = db_cursor.fetchall()
 
-        # Iterate list of data returned from database
+        # Iterate list of data returned from database line 124 is a "for loop"
         for row in dataset:
 
             # Create an animal instance from the current row.
             # Note that the database fields are specified in
             # exact order of the parameters defined in the
             # Animal class above.
-            animal = Animal(row['id'], row['name'], row['breed'],
-                            row['status'], row['location_id'],
-                            row['customer_id'])
+            # animal = Animal(row['id'], row['name'], row['breed'],
+            #                 row['status'], row['location_id'],
+            #                 row['customer_id'])
 
-            animals.append(animal.__dict__)
+            # animals.append(animal.__dict__)
+
+            # Create an animal instance from the current row
+            animal = Animal(row['id'], row['name'], row['breed'], row['status'],
+                            row['location_id'], row['customer_id'])
+
+            # Create a Location instance from the current row
+            location = Location(row['id'], row['location_name'], row['location_address'])
+
+            # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
+
+            # Add the dictionary representation of the animal to the list
+            animals.append(animal.__dict__)          
 
     # Use `json` package to properly serialize list as JSON
     return json.dumps(animals)
